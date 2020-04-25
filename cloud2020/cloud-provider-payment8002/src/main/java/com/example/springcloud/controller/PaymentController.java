d0bcd9465e06dabf23f8f0ef3e4c00d07dfeda6e
DropYearning <brtcho@yandex.com>
2020-04-25 20:44:28 +0800

    add SpringCloud 生产者集群2

---
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/example/springcloud/controller/PaymentController.java
@@ -0,0 +1,45 @@
+package com.example.springcloud.controller;
+
+import com.example.springcloud.entities.CommonResult;
+import com.example.springcloud.entities.Payment;
+import com.example.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController // Spring4之后新加入的注解，原来返回json.相当于@ResponseBody和@Controller配合。
+@Slf4j // 如果不想每次都写private final Logger logger = LoggerFactory.getLogger(当前类名.class); 可以用注解@Slf4j;
+
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}") // 可以读取配置文件中${server.port}的值， 目的是在结果页面显示本次查询由哪一台服务器提供
+    private String serverPort;
+
+    @PostMapping(value = "/payment/create") // 写操作Post
+    //@RequestBody 注解用于将Controller的方法参数，根据HTTP Request Header的content-Type的内容,通过适当的HttpMessageConverter转换为JAVA类
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果:" + result);
+        if (result > 0) {
+            return new CommonResult(200, "serverPort:" + serverPort + "插入数据成功", result);
+        }else {
+            return new CommonResult(444, "serverPort:" + serverPort + "插入数据失败", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}") // 查询Get
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        Payment result = paymentService.getPaymentById(id);
+        log.info("*****查询结果:" + result);
+        if (result != null ) {
+            return new CommonResult(200, "serverPort:" + serverPort + "查询数据成功", result);
+        }else {
+            return new CommonResult(444, "serverPort:" + serverPort + "没有对应记录, 查询id:" + id, null);
+        }
+    }
+}
