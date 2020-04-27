5d116b28cdbd0cd91993acf4dd1afc4306d5acab
DropYearning <brtcho@yandex.com>
2020-04-27 21:27:56 +0800

    add SpringCloud整合Hystrix实现服务端服务降级和熔断

--- cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/example/springcloud/HystrixPaymentMain8001.java
+++ cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/example/springcloud/HystrixPaymentMain8001.java
@@ -12,4 +12,14 @@ public class HystrixPaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixPaymentMain8001.class, args);
     }
+
+    //@Bean
+    //public ServletRegistrationBean getServlet() {
+    //    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+    //    ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+    //    registrationBean.setLoadOnStartup(1);
+    //    registrationBean.addUrlMappings("/hystrix.stream");
+    //    registrationBean.setName("HystrixMetricsStreamServlet");
+    //    return registrationBean;
+    //}
 }
