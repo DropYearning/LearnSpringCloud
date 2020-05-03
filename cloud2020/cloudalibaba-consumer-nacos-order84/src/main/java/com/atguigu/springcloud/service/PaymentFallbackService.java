f55a26da3d6f900312f816ca290a309d61529d11
DropYearning <brtcho@yandex.com>
2020-05-03 22:03:06 +0800

    add 演示Sentinel服务熔断（消费者模块84）

---
+++ cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.service;
+
+
+import com.example.springcloud.entities.CommonResult;
+import com.example.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentService {
+
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id) {
+        return new CommonResult<>(444, "服务降级 ---- fallback");
+    }
+
+}
