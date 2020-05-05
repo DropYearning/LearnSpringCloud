6f86c3d28d7560e568306b958f4aa0023320110a
DropYearning <brtcho@yandex.com>
2020-05-05 20:10:18 +0800

    update: Sentinel fallback、服务熔断

--- cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
+++ cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
@@ -7,10 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentFallbackService implements PaymentService {
-
     @Override
     public CommonResult<Payment> paymentSQL(Long id) {
         return new CommonResult<>(444, "服务降级 ---- fallback");
     }
-
 }
--- cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+// Feign使用接口 + 注解的方式实现远程调用
+// fallback指定服务降级时调用的类
 @FeignClient(value = "nacos-payment-provider", fallback = PaymentFallbackService.class)
 public interface PaymentService {
 
