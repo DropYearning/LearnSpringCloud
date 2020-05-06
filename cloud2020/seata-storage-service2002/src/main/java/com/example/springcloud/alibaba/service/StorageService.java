1e0f596e551f261ea14cb6c041a30e56eea7f5a1
DropYearning <brtcho@yandex.com>
2020-05-06 23:54:50 +0800

    add Seata库存模块2001

---
+++ cloud2020/seata-storage-service2002/src/main/java/com/example/springcloud/alibaba/service/StorageService.java
@@ -0,0 +1,5 @@
+package com.example.springcloud.alibaba.service;
+
+public interface StorageService {
+    void decrease(Long productId, Integer count);
+}
---
+++ cloud2020/seata-storage-service2002/src/main/java/com/example/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.springcloud.alibaba.service.impl;
+
+import com.example.springcloud.alibaba.dao.StorageDao;
+import com.example.springcloud.alibaba.service.StorageService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class StorageServiceImpl implements StorageService {
+
+    @Resource
+    private StorageDao storageDao;
+
+    /**
+     * 减库存
+     * @param productId
+     * @param count
+     * @return
+     */
+    @Override
+    public void decrease(Long productId, Integer count) {
+        storageDao.decrease(productId, count);
+    }
+}
